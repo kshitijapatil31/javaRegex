23337d1ae4132380bb605d6334c795afbae4e3f3
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-27 15:46:01 +0530

    [kshitija]:expression is clsed with supporting optional part

--- src/com/bridgelabz/email/Email.java
+++ src/com/bridgelabz/email/Email.java
@@ -4,7 +4,7 @@ public class Email {
 
 	public static void main(String[] args) {
 		
-		String regex1="^[a-z]{3}(.+)[a-zA-z_+-]{0,8}@[a-zA-Z]{3,20}(.+)$";
+		String regex1="^[a-z]{3}(.+)[a-zA-z_+-]{0,8}@([a-zA-Z]{3,14}(.+))([a-zA-z]{2})$";
 		String str="abc.xy_z@Bridgelabz.co";
        System.out.println(str.matches(regex1));
 	}
