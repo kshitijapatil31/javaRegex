399ac9b531e8ba5b66fff181ab14f2031f121132
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-27 14:28:11 +0530

    [kshitija]:whitespace is added betweenn pincode

--- src/com/bridgelabz/pincode/PinCode.java
+++ src/com/bridgelabz/pincode/PinCode.java
@@ -3,8 +3,8 @@ package com.bridgelabz.pincode;
 public class PinCode {
 
 	public static void main(String[] args) {
-		String regex="^[0-9]{6}$";
-		String str="400008B";
+		String regex="^[0-9]{3}\\s{0,1}[0-9]{3}$";
+		String str="400 008";
         System.out.println(str.matches(regex));
 	}
 
