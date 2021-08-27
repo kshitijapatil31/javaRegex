5d4ea49adb43e5ed3e388dbf098c944f594a19e4
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-27 14:44:34 +0530

    [kshitija]:usecase 1 of email is solved

---
+++ src/com/bridgelabz/email/Email.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.email;
+
+public class Email {
+
+	public static void main(String[] args) {
+		String regex="^[a-z]{3}";
+		String str="abc";
+ System.out.println(str.matches(regex));
+	}
+
+}
