ce8d6fdea304106ac0d6eacd95ebe51eb8839e96
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-27 14:02:15 +0530

    [kshitija]:pincode is restricted from takin alphabets or special character

--- src/com/bridgelabz/pincode/PinCode.java
+++ src/com/bridgelabz/pincode/PinCode.java
@@ -3,8 +3,8 @@ package com.bridgelabz.pincode;
 public class PinCode {
 
 	public static void main(String[] args) {
-		String regex="[0-9]{6}";
-		String str="400008";
+		String regex="^[0-9]{6}";
+		String str="A400008";
         System.out.println(str.matches(regex));
 	}
 
